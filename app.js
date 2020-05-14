0c1a92ba7f30ba08dd98a3f6df97b26992cb3763
joasog <joachim.sogn@gmail.com>
2020-05-14 13:26:20 +0200

    easy start of express.js with two middlewares

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const express = require('express');
 // starting th
 const app = express();
 
-const routes = require('./routes')
-
 const server = http.createServer(app)
+
+// This funciton will be executed for every incoming request
+// Must call next for the next app.use to be called
+
+app.use((req,res,next)=>{
+    console.log('in the middleware')
+    next()
+
+})
+
+app.use((req,res,next)=>{
+    console.log('another middleware')
+
+})
 
 server.listen(3000);
 
+//easy server
 
