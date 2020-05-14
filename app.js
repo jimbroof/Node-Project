7a63161580ed72573d846eeff0eb5e1b8f257d26
joasog <joachim.sogn@gmail.com>
2020-05-14 13:49:42 +0200

    three middlewares

--- app.js
+++ app.js
@@ -7,18 +7,27 @@ const app = express();
 
 const server = http.createServer(app)
 
-// All routes that start with /
-app.use('/products', (req,res,next)=>{
-    console.log('another middleware')
-    res.send('<h1>This is the Product page</h1>')
-})
-
 
 // All routes that start with /
 app.use('/', (req,res,next)=>{
-    console.log('another middleware')
+    console.log('This always runs')
+    next()
+})
+
+// All routes that start with /
+app.use('/products', (req,res,next)=>{
+    console.log('This runs on /products')
+    res.send('<h1>This is the Product page</h1>')
+})
+
+// All routes that start with /
+app.use('/', (req,res,next)=>{
+    console.log('this runs on /')
     res.send('<h1>Hello from express</h1>')
 })
+
+
+
 
 app.listen(3000);
 
