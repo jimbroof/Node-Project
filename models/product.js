bd7109426875165acf8b87414224071c019f9911
joasog <joachim.sogn@gmail.com>
2020-05-22 15:29:38 +0200

    changed a few things with callback functions

--- models/product.js
+++ models/product.js
@@ -2,6 +2,23 @@
 const fs = require('fs');
 const path = require('path');
 
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+  );
+
+const getProductsFromFile = (cb) =>{
+
+      fs.readFile(p, (err, fileContent) => {
+        if (err) {
+          return cb([]);
+        }
+        return cb(JSON.parse(fileContent));
+      });
+}
+
 module.exports = class Product{
 
     constructor(title){
@@ -9,34 +26,17 @@ module.exports = class Product{
     }
 
     save() {
-        const p = path.join(
-          path.dirname(process.mainModule.filename),
-          'data',
-          'products.json'
-        );
+        getProductsFromFile( (products) =>{
+            
+            products.push(this);
 
-        fs.readFile(p, (err, fileContent) => {
-          let products = [];
-          if (!err) {
-            products = JSON.parse(fileContent);
-          }
-          products.push(this);
-          fs.writeFile(p, JSON.stringify(products), err => {
-            console.log(err);
-          });
+                fs.writeFile(p, JSON.stringify(products), err => {
+                  console.log(err);
+                });
+            
         });
       }
  static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb)
   }
 }
